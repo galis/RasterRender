61082f7b1561e0298c34649c07db6e40068c46fe
Galis <591817405@qq.com>
2022-08-20 15:38:15 +0800

    添加效果图

--- main.cpp
+++ main.cpp
@@ -9,19 +9,17 @@
 
 int main() {
     Model model("/Volumes/mac-code/RasterizationRender/assets/cyborg/cyborg.obj");
-//    Model model("/Volumes/mac-code/RasterizationRender/assets/backpack/backpack.obj");
     model.load();
     Rasterizer rasterizer;
     glm::vec4 viewport(0, 0, WIDTH, HEIGHT);
     glm::mat4x4 modelMatrix = glm::translate(glm::identity<glm::mat4x4>(), {0.0, 0, 0});
-    glm::mat4x4 viewMatrix = glm::lookAt(glm::vec3(0, 0, 10), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+    glm::mat4x4 viewMatrix = glm::lookAt(glm::vec3(-10, 0, -10), glm::vec3(0, 1, 0), glm::vec3(0, 1, 0));
     glm::mat4x4 projMatrix = glm::perspective(45.f, 1.f, 0.1f, 100.f);
     rasterizer.setModelMatrix(modelMatrix);
     rasterizer.setViewMatrix(viewMatrix);
     rasterizer.setProjectionMatrix(projMatrix);
     rasterizer.setViewPort(viewport);
     double t = (double) getTickCount();
-    // do something ...
     Mat mat = rasterizer.render(model);
     t = ((double) getTickCount() - t) / getTickFrequency();
     cv::putText(mat, "UseTime:" + to_string(t), Point(100, 100), 0, 1, Scalar(255, 0, 0));
